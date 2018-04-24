563bf5f26de1908d631dae26844dcaa7365ec076
dahby <dstoll243@gmail.com>
2018-04-24 15:44:38 -0700

    test passing for GET request to /

---
+++ src/__test__/server.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const server = require('../lib/server');
+const superagent = require('superagent');
+// const cowsay = require('cowsay');
+
+beforeAll(() => server.start(5000));
+afterAll(() => server.stop());
+
+describe('VALID requests to the API', () => {
+  describe('GET /', () => {
+    test('response should have status 200', () => {
+      return superagent.get(':5000/').then((res) => {
+        expect(res.status).toEqual(200);
+      });
+    });
+  });
+});
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,17 +1,32 @@
 'use strict';
 
 const http = require('http');
-const cowsay = require('cowsay');
+// const cowsay = require('cowsay');
 const bodyParser = require('./body-parser');
 
 const server = module.exports = {};
 
 const app = http.createServer((req, res) => {
-  bodyParser(req).then((parsedRequest) => {
-    if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-    }
-  });
+  bodyParser(req)
+    .then((parsedRequest) => {
+      if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<body><header><nav><ul><li><a href="/cowsay">cowsay</a></li></ul></nav></header><main>Here there be a description</main></body>');
+        res.end();
+        return undefined;
+      }
+
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('NOT FOUND');
+      res.end();
+      return undefined;
+    })
+    .catch((err) => {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write('BAD REQUEST', err);
+      res.end();
+      return undefined;
+    });
 });
 
 
