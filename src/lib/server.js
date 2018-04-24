c88ed2a359972fdf90e60477eadbb4452faf7493
dahby <dstoll243@gmail.com>
2018-04-24 16:23:25 -0700

    2nd feature task working

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const http = require('http');
-// const cowsay = require('cowsay');
+const cowsay = require('cowsay');
 const bodyParser = require('./body-parser');
+const faker = require('faker');
 
 const server = module.exports = {};
 
@@ -12,6 +13,18 @@ const app = http.createServer((req, res) => {
       if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<body><header><nav><ul><li><a href="/cowsay">cowsay</a></li></ul></nav></header><main>Here there be a description</main></body>');
+        res.end();
+        return undefined;
+      }
+
+      if (parsedRequest.method === 'GET' && parsedRequest.url.pathname === '/cowsay') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        let msg = parsedRequest.url.query.text;
+        if (!parsedRequest.url.query.text) {
+          msg = faker.random.word();
+        }
+        const cowsayMessage = cowsay.say({ text: msg });
+        res.write(`<body><h1> cowsay </h1><pre>${cowsayMessage}</pre></body>`);
         res.end();
         return undefined;
       }
